5e122a93a7fbbfeb4e6022371bc01ecf0de6edd5
Michael Marshall <michaelmarshall220@gmail.com>
2021-09-23 16:03:40 -0500

    removed global cors to make serving static files easier. cors on controllers

--- api/src/main/kotlin/com/seedcompany/cordspringstencil/CordSpringStencilApplication.kt
+++ api/src/main/kotlin/com/seedcompany/cordspringstencil/CordSpringStencilApplication.kt
@@ -1,31 +1,11 @@
 package com.seedcompany.cordspringstencil
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Configuration
-import org.springframework.scheduling.annotation.EnableScheduling
-import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.EnableWebMvc
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
-
 
 @SpringBootApplication
-//@ConfigurationPropertiesScan
-//@EnableScheduling
 class CordSpringStencilApplication
 
 fun main(args: Array<String>) {
     runApplication<CordSpringStencilApplication>(*args)
 }
-
-//@Configuration
-//@EnableWebMvc
-//class WebConfig : WebMvcConfigurer {
-//    override fun addCorsMappings(registry: CorsRegistry) {
-//        registry.addMapping("/**")
-//            .allowedOrigins("*")
-//            .allowedHeaders("*")
-//            .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
-//    }
-//}
