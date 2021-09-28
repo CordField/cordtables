6028714ccdf0b40eaa315a63f24cd3c201bde8eb
Michael Marshall <michaelmarshall220@gmail.com>
2021-09-28 18:04:55 -0500

    updated bootstrap file to create admin role

--- infra/local/env.sh
+++ infra/local/env.sh
@@ -2,4 +2,5 @@ export DB_DOMAIN=localhost
 export DB_DATABASE=cordfield
 export DB_USERNAME=postgres
 export DB_PASSWORD=asdfasdf
-export SERVER_URL="http://localhost:8080"
+export SERVER_URL="http://localhost:8080/"
+export CORD_ADMIN_PASSWORD=asdfasdf
