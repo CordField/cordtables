46bd3ce3ef13ae20ce3e213dfe60353251095863
Michael Marshall <michaelmarshall220@gmail.com>
2021-09-28 21:28:40 -0500

    testcontainers passing bootstrap

--- api/src/test/kotlin/com/seedcompany/cordspringstencil/CordSpringStencilApplicationTests.kt
+++ api/src/test/kotlin/com/seedcompany/cordspringstencil/CordSpringStencilApplicationTests.kt
@@ -1,13 +1,53 @@
 package com.seedcompany.cordspringstencil
 
 import org.junit.jupiter.api.Test
+import org.openqa.selenium.chrome.ChromeOptions
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.web.server.LocalServerPort
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import org.testcontainers.containers.BrowserWebDriverContainer
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
 
-@SpringBootTest
-class CordSpringStencilApplicationTests {
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class CordSpringStencilApplicationTests(
+	@LocalServerPort
+	val port: Int
+) {
+
+	@Container
+	private val container: BrowserWebDriverContainer<*> = BrowserWebDriverContainer<Nothing>()
+		.withCapabilities(ChromeOptions())
+
+	companion object {
+		@Container
+		private val postgreSQLContainer = PostgreSQLContainer<Nothing>("postgres:latest").apply {
+			withUsername("postgres")
+			withPassword("asdfasdf")
+			withDatabaseName("cordfield")
+		}
+
+		@DynamicPropertySource
+		@JvmStatic
+		fun registerDynamicProperties(registry: DynamicPropertyRegistry) {
+			System.setProperty("CORD_ADMIN_PASSWORD", "asdfasdf")
+			System.setProperty("DB_DOMAIN", "host.docker.internal")
+			System.setProperty("DB_DATABASE", "cordfield")
+			System.setProperty("DB_USERNAME", "postgres")
+			System.setProperty("DB_PASSWORD", "asdfasdf")
+
+			registry.add("spring.datasource.jdbcUrl", postgreSQLContainer::getJdbcUrl)
+			registry.add("spring.datasource.username", postgreSQLContainer::getUsername)
+			registry.add("spring.datasource.password", postgreSQLContainer::getPassword)
+		}
+	}
 
 	@Test
 	fun contextLoads() {
+		assert(true)
 	}
 
 }
