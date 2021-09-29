eaa7be8f22fa1c3d20198e0de78c443dd667bfed
Michael Marshall <michaelmarshall220@gmail.com>
2021-09-28 21:41:45 -0500

    added hello world selenium test

--- api/src/test/kotlin/com/seedcompany/cordspringstencil/CordSpringStencilApplicationTests.kt
+++ api/src/test/kotlin/com/seedcompany/cordspringstencil/CordSpringStencilApplicationTests.kt
@@ -14,40 +14,56 @@ import org.testcontainers.junit.jupiter.Testcontainers
 @Testcontainers
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CordSpringStencilApplicationTests(
-	@LocalServerPort
-	val port: Int
+    @LocalServerPort
+    val port: Int
 ) {
 
-	@Container
-	private val container: BrowserWebDriverContainer<*> = BrowserWebDriverContainer<Nothing>()
-		.withCapabilities(ChromeOptions())
+    @Container
+    private val container: BrowserWebDriverContainer<*> = BrowserWebDriverContainer<Nothing>()
+        .withCapabilities(ChromeOptions())
 
-	companion object {
-		@Container
-		private val postgreSQLContainer = PostgreSQLContainer<Nothing>("postgres:latest").apply {
-			withUsername("postgres")
-			withPassword("asdfasdf")
-			withDatabaseName("cordfield")
-		}
+    companion object {
+        @Container
+        private val postgreSQLContainer = PostgreSQLContainer<Nothing>("postgres:latest").apply {
+            withUsername("postgres")
+            withPassword("asdfasdf")
+            withDatabaseName("cordfield")
+        }
 
-		@DynamicPropertySource
-		@JvmStatic
-		fun registerDynamicProperties(registry: DynamicPropertyRegistry) {
-			System.setProperty("CORD_ADMIN_PASSWORD", "asdfasdf")
-			System.setProperty("DB_DOMAIN", "host.docker.internal")
-			System.setProperty("DB_DATABASE", "cordfield")
-			System.setProperty("DB_USERNAME", "postgres")
-			System.setProperty("DB_PASSWORD", "asdfasdf")
+        @DynamicPropertySource
+        @JvmStatic
+        fun registerDynamicProperties(registry: DynamicPropertyRegistry) {
+            System.setProperty("CORD_ADMIN_PASSWORD", "asdfasdf")
+            System.setProperty("DB_DOMAIN", "host.docker.internal")
+            System.setProperty("DB_DATABASE", "cordfield")
+            System.setProperty("DB_USERNAME", "postgres")
+            System.setProperty("DB_PASSWORD", "asdfasdf")
 
-			registry.add("spring.datasource.jdbcUrl", postgreSQLContainer::getJdbcUrl)
-			registry.add("spring.datasource.username", postgreSQLContainer::getUsername)
-			registry.add("spring.datasource.password", postgreSQLContainer::getPassword)
-		}
-	}
+            registry.add("spring.datasource.jdbcUrl", postgreSQLContainer::getJdbcUrl)
+            registry.add("spring.datasource.username", postgreSQLContainer::getUsername)
+            registry.add("spring.datasource.password", postgreSQLContainer::getPassword)
+        }
+    }
 
-	@Test
-	fun contextLoads() {
-		assert(true)
-	}
+    @Test
+    fun `doesThisWork`() {
+
+        container.webDriver["http://host.docker.internal:$port/"]
+
+        Thread.sleep(5000)
+
+        val messageElement = container.webDriver.findElementByTagName("app-root")
+
+        println("app-root: ${messageElement.toString()}")
+
+        assert(true)
+
+//    Assertions.assertEquals("Integration Test with Selenium", messageElement.text)
+    }
+
+    @Test
+    fun contextLoads() {
+        assert(true)
+    }
 
 }
