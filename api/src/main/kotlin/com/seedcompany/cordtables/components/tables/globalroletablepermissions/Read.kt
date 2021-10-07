54eb7fff8b41517a2fef18c319eeb2c9c9770d50
Parashara1 <parashara@vcu.edu>
2021-10-07 14:59:05 -0400

    "kotlin files for global_role_table_permissions, update still needs work"

---
+++ api/src/main/kotlin/com/seedcompany/cordtables/components/tables/globalroletablepermissions/Read.kt
@@ -0,0 +1,74 @@
+package com.seedcompany.cordtables.components.user
+
+import com.seedcompany.cordtables.common.ErrorType
+import com.seedcompany.cordtables.common.Utility
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.ResponseBody
+import java.sql.SQLException
+import javax.sql.DataSource
+
+
+data class GlobalRolesTablePermissions(
+    val id: Int,
+    val createdAt: String,
+    val createdBy: Int,
+    val modifiedAt: String,
+    val modifiedBy: Int,
+    val tableName: String,
+    val tablePermission: String,
+    val globalRole: Int
+
+)
+
+data class ReadPermissionsResponse(
+    val error: ErrorType,
+    val data: MutableList<GlobalRolesTablePermissions>?
+    // val token: String? = null,
+)
+
+@CrossOrigin(origins = ["http://localhostt:3333"])
+@Controller()
+class Read (
+    @Autowired
+    val util: Utility,
+
+    @Autowired
+    val ds: DataSource
+) {
+    @PostMapping("globalrolestablepermissions/read")
+    @ResponseBody
+    fun ReadHandler(): ReadPermissionsResponse {
+
+        var data: MutableList<GlobalRolesTablePermissions> = mutableListOf()
+
+        this.ds.connection.use { conn ->
+            val listStatement = conn.prepareCall(
+                "select id, created_at, created_by, modified_at, modified_by, table_name, table_permission, global_role from public.global_roles_table_permissions;"
+            )
+            try {
+
+                val listStatementResult = listStatement.executeQuery()
+                while (listStatementResult.next()) {
+                    val id = listStatementResult.getInt("id")
+                    val createdAt = listStatementResult.getString("created_at")
+                    val createdBy = listStatementResult.getInt("created_by")
+                    val modifiedBy = listStatementResult.getInt("modified_by")
+                    val modifiedAt = listStatementResult.getString("modified_at")
+                    val tableName = listStatementResult.getString("table_name")
+                    val tablePermissions = listStatementResult.getString("table_permissions")
+                    val globalRole = listStatementResult.getInt("global_role")
+                    data.add(GlobalRolesTablePermissions(id,createdAt,createdBy,modifiedAt,modifiedBy,tableName,tablePermissions,globalRole))
+                }
+            }
+            catch (e:SQLException){
+                println("error while listing ${e.message}")
+                return ReadPermissionsResponse(ErrorType.SQLReadError, null)
+
+            }
+        }
+        return ReadPermissionsResponse(ErrorType.NoError,data)
+    }
+}
---
+++ api/src/main/kotlin/com/seedcompany/cordtables/components/tables/globalroletablepermissions/Update.kt
@@ -0,0 +1,86 @@
+package com.seedcompany.cordtables.components.tables.globalroles
+
+import com.seedcompany.cordtables.common.ErrorType
+import com.seedcompany.cordtables.common.Utility
+import com.seedcompany.cordtables.components.user.GlobalRolesTablePermissions
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.ResponseBody
+import java.sql.SQLException
+import java.sql.Time
+import java.sql.Timestamp
+import javax.sql.DataSource
+import kotlin.reflect.full.memberProperties
+import kotlin.collections.mutableListOf as mutableListOf
+import java.time.Instant
+
+data class UpdatePermissionsResponse(
+    val error: ErrorType,
+    val globalRolesTablePermissions: GlobalRolesTablePermissions?
+)
+
+data class UpdatePermissionsRequest(
+    val tableName: String,
+    val globalRole: Int,
+    val tablePermissions: String,
+    val email: String,
+    val id: Int
+)
+
+@CrossOrigin(origins = ["http://localhost:3333"])
+@Controller()
+class Update(
+    @Autowired
+    val util: Utility,
+
+    @Autowired
+    val ds: DataSource,
+) {
+    @PostMapping("globalrolestablepermissions/updatte")
+    @ResponseBody
+    fun UpdateHandler(@RequestBody req: UpdatePermissionsRequest): UpdatePermissionsResponse {
+
+        println("req: $req")
+        var updatedGRTPermissions: GlobalRolesTablePermissions? = null
+        var userId = 0
+
+        this.ds.connection.use { conn ->
+            try {
+                val getUserIdStatement = conn.prepareCall("select person from public.users where email = ?")
+                getUserIdStatement.setString(1, req.email)
+                val getUserIdResult = getUserIdStatement.executeQuery()
+                if (getUserIdResult.next()) {
+                    userId = getUserIdResult.getInt("person")
+                    println("userId: $userId")
+                } else {
+                    throw SQLException("User not found")
+                }
+            } catch(e:SQLException){
+                println(e.message)
+                return UpdatePermissionsResponse(ErrorType.UserNotFound, null)
+            }
+            try {
+
+                var reqValues: MutableList<Any> = mutableListOf()
+                var updateSQL = "update public.global_roles_table_permissions set"
+                for (prop in UpdatePermissionsRequest::class.memberProperties) {
+                    val propValue = prop.get(req)
+                    println("$propValue $(prop.name) = ?,")
+
+                }
+
+            }
+            catch (e:SQLException){
+                println(e.message)
+                errorType = ErrorType.SQLInsertError
+                return CreateGRTPermissionsResponse(errorType, null)
+
+            }
+        }
+        return CreateGRTPermissionsResponse(ErrorType.NoError, newGRTPermissions)
+
+    }
+}
