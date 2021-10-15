fd8a61978f8616c965b3154e63fc5cf498ef57b2
Michael Marshall <michaelmarshall220@gmail.com>
2021-10-15 16:56:34 -0500

    starting to cycle through language ethnologue entries

--- api/src/main/kotlin/com/seedcompany/cordtables/components/tables/sil/tableoflanguages/Migrate.kt
+++ api/src/main/kotlin/com/seedcompany/cordtables/components/tables/sil/tableoflanguages/Migrate.kt
@@ -4,6 +4,7 @@ import com.seedcompany.cordtables.common.CordApiRestUtils
 import com.seedcompany.cordtables.common.Utility
 import com.seedcompany.cordtables.core.AppConfig
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.DependsOn
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
@@ -13,20 +14,41 @@ import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.client.RestTemplate
 import javax.sql.DataSource
 
+data class ValueInt (
+    val value: Int? = null,
+)
+data class ValueString (
+    val value: String? = null,
+)
+
+data class Ethnologue(
+    val code: ValueString? = null,
+    val name: ValueString? = null,
+    val population: ValueInt? = null,
+    val provisionalCode: ValueString? = null,
+    val sensitivity: String? = null,
+)
+data class Item(
+    val id: String? = null,
+    val ethnologue: Ethnologue? = null,
+)
+
 data class Languages(
-    val total: Int?,
+    val items: List<Item>? = listOf(),
+    val total: Int? = null,
 )
 
 data class Data(
-    val languages: Languages?,
+    val languages: Languages? = null,
 )
 
 data class Response(
-    val data: Data,
+    val data: Data? = null,
 )
 
 @CrossOrigin(origins = ["http://localhost:3333", "https://dev.cordtables.com", "https://cordtables.com"])
 @Controller("SilTableOfLanguagesMigrate")
+@DependsOn("BootstrapDB")
 class Migrate(
     @Autowired
     val util: Utility,
@@ -54,14 +76,25 @@ class Migrate(
 
             // get count of ethnologue entries
             val countQuery = """{"query":"query Query {languages {total}}"}"""
-
             val countResponse = cord.post<Response>(token, countQuery)
-
             val total = countResponse?.data?.languages?.total
 
-            println("total languages: $total")
+            if (total != null) {
+                println("total languages: $total")
+
+                // iterate over all languages and ensure we have them in postgres
+                for (i in 1..total) {
+
+                    val ethQuery = """
+                        {"query":"query Query {languages(input: { page: $i, count: 1 }) {items {id ethnologue {code {value}name {value}population {value} provisionalCode {value} sensitivity}}}}"}
+                    """.trimIndent()
+
+                    val ethResponse = cord.post<Response>(token, ethQuery)
 
 
+
+                }
+            }
 
         }
 
