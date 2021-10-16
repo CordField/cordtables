dd0783e42cd29c2e2d9754bf3c09dc96aef10f15
Michael Marshall <michaelmarshall220@gmail.com>
2021-10-16 15:00:33 -0500

    migration is now an endpoint.

--- api/src/main/kotlin/com/seedcompany/cordtables/components/tables/sil/tableoflanguages/Migrate.kt
+++ api/src/main/kotlin/com/seedcompany/cordtables/components/tables/sil/tableoflanguages/Migrate.kt
@@ -3,6 +3,8 @@ package com.seedcompany.cordtables.components.tables.sil.tableoflanguages
 import com.seedcompany.cordtables.common.CordApiRestUtils
 import com.seedcompany.cordtables.common.ErrorType
 import com.seedcompany.cordtables.common.Utility
+import com.seedcompany.cordtables.components.user.RegisterRequest
+import com.seedcompany.cordtables.components.user.RegisterReturn
 import com.seedcompany.cordtables.core.AppConfig
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.DependsOn
@@ -10,6 +12,9 @@ import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.client.RestTemplate
 import javax.sql.DataSource
 
@@ -70,7 +75,10 @@ class Migrate(
     val jdbcTemplate: NamedParameterJdbcTemplate = NamedParameterJdbcTemplate(ds)
     val jdbcTemplate2 = JdbcTemplate(ds)
 
-    init {
+
+    @PostMapping("migrate/sc-ethnologue")
+    @ResponseBody
+    fun registerHandler() {
 
         val token = cord.login()
 
@@ -81,56 +89,63 @@ class Migrate(
             val countResponse = cord.post<Response>(token, countQuery)
             val total = countResponse?.data?.languages?.total
 
-            if (total != null) {
-                println("total languages: $total")
+            if (total == null) {
+                println("language total was null")
+                return
+            }
 
-                // iterate over all languages and ensure we have them in postgres
-                for (i in 1..total) {
+            println("total languages: $total")
 
-                    val ethQuery = """
+            // iterate over all languages and ensure we have them in postgres
+            for (i in 1..total) {
+
+                val ethQuery = """
                         {"query":"query Query {languages(input: { page: $i, count: 1 }) {items {id ethnologue {code {value}name {value}population {value} provisionalCode {value} sensitivity}}}}"}
                     """.trimIndent()
 
-                    val ethResponse = cord.post<Response>(token, ethQuery)
+                val ethResponse = cord.post<Response>(token, ethQuery)
 
-                    val eth = ethResponse?.data?.languages?.items?.get(0)?.ethnologue
-                    if (eth != null) {
+                val eth = ethResponse?.data?.languages?.items?.get(0)?.ethnologue
+                if (eth == null) {
+                    println("ethnologue entry was null")
+                    continue
+                }
 
-                        var errorType: ErrorType? = null
+                var errorType: ErrorType? = null
 
-                        this.ds.connection.use { conn ->
-                            val migrationStatement = conn.prepareCall("call sc.sc_migrate_ethnologue(?,?,?,?,?,?);")
-                            migrationStatement.setString(1, eth.code?.value)
-                            migrationStatement.setString(2, eth.name?.value)
-                            if (eth.population?.value != null) {
-                                migrationStatement.setInt(3, eth.population?.value)
-                            } else {
-                                migrationStatement.setNull(3, java.sql.Types.NULL)
-                            }
-                            migrationStatement.setString(4, eth.provisionalCode?.value)
-                            migrationStatement.setObject(5, eth.sensitivity, java.sql.Types.OTHER)
-                            migrationStatement.registerOutParameter(6, java.sql.Types.VARCHAR)
+                this.ds.connection.use { conn ->
+                    val migrationStatement = conn.prepareCall("call sc.sc_migrate_ethnologue(?,?,?,?,?,?);")
+                    migrationStatement.setString(1, eth.code?.value)
+                    migrationStatement.setString(2, eth.name?.value)
+                    if (eth.population?.value != null) {
+                        migrationStatement.setInt(3, eth.population?.value)
+                    } else {
+                        migrationStatement.setNull(3, java.sql.Types.NULL)
+                    }
+                    migrationStatement.setString(4, eth.provisionalCode?.value)
+                    migrationStatement.setObject(5, eth.sensitivity, java.sql.Types.OTHER)
+                    migrationStatement.registerOutParameter(6, java.sql.Types.VARCHAR)
 
-                            migrationStatement.execute()
+                    migrationStatement.execute()
 
-                            try {
-                                errorType = ErrorType.valueOf(migrationStatement.getString(6))
-                            } catch (ex: IllegalArgumentException) {
-                                errorType = ErrorType.UnknownError
-                            }
-
-                            if (errorType != ErrorType.NoError) {
-                                println("ethnologue migration query failed")
-                            }
-
-                            migrationStatement.close()
-                        }
-
-                        println("Migrated ethnologue entry code: '${eth.code?.value}': $errorType")
+                    try {
+                        errorType = ErrorType.valueOf(migrationStatement.getString(6))
+                    } catch (ex: IllegalArgumentException) {
+                        errorType = ErrorType.UnknownError
                     }
 
+                    if (errorType != ErrorType.NoError) {
+                        println("ethnologue migration query failed")
+                    }
+
+                    migrationStatement.close()
                 }
+
+                println("Migrated ethnologue entry code: '${eth.code?.value}': $errorType")
+
+
             }
+
 
         }
 
