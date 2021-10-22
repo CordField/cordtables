9069edcbc287e48a5094cbe9095ca5757aeca3d9
Michael Marshall <michaelmarshall220@gmail.com>
2021-10-22 10:50:37 -0500

    wip

--- frontend/src/common/table-abstractions/cf-cell/cf-cell.tsx
+++ frontend/src/common/table-abstractions/cf-cell/cf-cell.tsx
@@ -1,12 +1,11 @@
 import { Component, Host, h } from '@stencil/core';
 
 @Component({
-  tag: 'cf-cell',
+  tag: 'cf-cell2',
   styleUrl: 'cf-cell.css',
   shadow: true,
 })
 export class CfCell {
-
   render() {
     return (
       <Host>
@@ -14,5 +13,4 @@ export class CfCell {
       </Host>
     );
   }
-
 }
