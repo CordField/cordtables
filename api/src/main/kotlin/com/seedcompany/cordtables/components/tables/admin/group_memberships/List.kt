4e09bc5527384c816cdc40693ae9f98e9c7cfb7d
Parashara1 <parashara@vcu.edu>
2021-10-22 05:33:00 -0400

    "Added latest secure list query & fixed package names"

--- api/src/main/kotlin/com/seedcompany/cordtables/components/tables/admin/group_memberships/List.kt
+++ api/src/main/kotlin/com/seedcompany/cordtables/components/tables/admin/group_memberships/List.kt
@@ -1,4 +1,4 @@
-package com.seedcompany.cordtables.components.tables.groupmemberships
+package com.seedcompany.cordtables.components.tables.admin.group_memberships
 
 import com.seedcompany.cordtables.common.ErrorType
 import com.seedcompany.cordtables.common.Utility
@@ -6,75 +6,144 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.PostMapping
+import com.seedcompany.cordtables.components.admin.GetSecureListQuery
+import com.seedcompany.cordtables.components.admin.GetSecureListQueryRequest
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.ResponseBody
+import java.sql.SQLException
 import javax.sql.DataSource
 import kotlin.collections.List
 
-data class GroupRowAccessRow(
-    val id: Int,
-    val group: Int,
-    val tableName: String,
-    val row: Int,
-    val createdAt: String,
-    val createdBy: Int,
-    val modifiedAt: String,
-    val modifiedBy: Int,
+data class GroupMembershipsRow(
+    val id: Int?,
+    val group: Int?,
+    val person: Int?,
+    val createdAt: String?,
+    val createdBy: Int?,
+    val modifiedAt: String?,
+    val modifiedBy: Int?,
 )
 
-data class GroupRowAccessListRequest(
+data class GroupMembershipsListRequest(
     val token: String? = null,
 )
 
-data class GroupRowAccessListReturn(
+data class GroupMembershipsListReturn(
     val error: ErrorType,
-    val groupRowAccessList: List<out GroupRowAccessRow>?,
+    val groupMemberships: List<out GroupMembershipsRow>?,
 )
 
 @CrossOrigin(origins = ["http://localhost:3333", "https://dev.cordtables.com", "https://cordtables.com"])
-@Controller("GroupRowAccessList")
+@Controller("GroupMembershipsList")
 class List(
     @Autowired
     val util: Utility,
 
     @Autowired
     val ds: DataSource,
+
+    @Autowired
+    val secureList: GetSecureListQuery,
 ) {
 
-    @PostMapping("grouprowaccess/list")
+    var jdbcTemplate: NamedParameterJdbcTemplate = NamedParameterJdbcTemplate(ds)
+
+    @PostMapping("group_memberships/list")
     @ResponseBody
-    fun listHandler(@RequestBody req: GroupRowAccessListRequest): GroupRowAccessListReturn {
+    fun listHandler(@RequestBody req: GroupMembershipsListRequest): GroupMembershipsListReturn {
 
-        if (req.token == null) return GroupRowAccessListReturn(ErrorType.TokenNotFound, null)
-        if (!util.isAdmin(req.token)) return GroupRowAccessListReturn(ErrorType.AdminOnly, null)
+        var items = mutableListOf<GroupMembershipsRow>()
+        if (req.token == null) return GroupMembershipsListReturn(ErrorType.TokenNotFound, null)
+        if (!util.isAdmin(req.token)) return GroupMembershipsListReturn(ErrorType.AdminOnly, null)
 
-        val items = mutableListOf<GroupRowAccessRow>()
+        val paramSource = MapSqlParameterSource()
+        paramSource.addValue("token", req.token)
 
-        this.ds.connection.use { conn ->
-            //language=SQL
-            val statement = conn.prepareStatement("""
-                select * from admin.group_row_access order by id asc;
-            """.trimIndent())
+        val query = secureList.getSecureListQueryHandler(
+            GetSecureListQueryRequest(
+                tableName = "admin.group_memberships",
+                columns = arrayOf(
+                    "id",
+                    "group",
+                    "person",
+                    "createdAt",
+                    "createdBy",
+                    "modifiedAt",
+                    "modifiedBy"
+                )
+            )
+        ).query
 
-            val result = statement.executeQuery()
+        try {
+            val jdbcResult = jdbcTemplate.queryForRowSet(query, paramSource)
+            while (jdbcResult.next()) {
+                var id: Int? = jdbcResult.getInt("id")
+                if (jdbcResult.wasNull()) id = null
 
-            while (result.next()){
+                var group: Int? = jdbcResult.getInt("group")
+                if (jdbcResult.wasNull()) group = null
+
+                var person: Int? = jdbcResult.getInt("person")
+                if (jdbcResult.wasNull()) person = null
+
+                var createdAt: String? = jdbcResult.getString("created_at")
+                if (jdbcResult.wasNull()) createdAt = null
+
+                var createdBy: Int? = jdbcResult.getInt("created_by")
+                if (jdbcResult.wasNull()) createdBy = null
+
+                var modifiedAt: String? = jdbcResult.getString("modified_at")
+                if (jdbcResult.wasNull()) modifiedAt = null
+
+                var modifiedBy: Int? = jdbcResult.getInt("modified_by")
+                if (jdbcResult.wasNull()) modifiedBy = null
+
                 items.add(
-                    GroupRowAccessRow(
-                        id = result.getInt("id"),
-                        group = result.getInt("group_id"),
-                        tableName = result.getString("table_name"),
-                        row = result.getInt("row"),
-                        createdAt = result.getString("created_at"),
-                        createdBy = result.getInt("created_by"),
-                        modifiedAt = result.getString("modified_at"),
-                        modifiedBy = result.getInt("modified_by"),
+                    GroupMembershipsRow(
+                        id = id,
+                        group = group,
+                        person = person,
+                        createdAt = createdAt,
+                        createdBy = createdBy,
+                        modifiedAt = modifiedAt,
+                        modifiedBy = modifiedBy
+
                     )
                 )
             }
+        } catch (e: SQLException) {
+            println("error while listing ${e.message}")
+            return GroupMembershipsListReturn(ErrorType.SQLReadError, mutableListOf())
         }
 
-        return GroupRowAccessListReturn(ErrorType.NoError, items)
+
+
+//        this.ds.connection.use { conn ->
+//            //language=SQL
+//            val statement = conn.prepareStatement("""
+//                select * from admin.group_memberships order by id asc;
+//            """.trimIndent())
+//
+//            val result = statement.executeQuery()
+//
+//            while (result.next()){
+//                items.add(
+//                    GroupMembershipsRow(
+//                        id = result.getInt("id"),
+//                        group = result.getInt("group_id"),
+//                        person = result.getInt("person"),
+//                        createdAt = result.getString("created_at"),
+//                        createdBy = result.getInt("created_by"),
+//                        modifiedAt = result.getString("modified_at"),
+//                        modifiedBy = result.getInt("modified_by"),
+//                    )
+//                )
+//            }
+//        }
+
+        return GroupMembershipsListReturn(ErrorType.NoError, items)
     }
 
 }
