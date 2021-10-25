8ac50ff14f1387b36c2c51b0b4ab87e923d51053
E10001204 <sorrelle04@gmail.com>
2021-10-25 13:09:46 -0500

    add more graphql shapes and add some organizational comments

--- api/src/main/kotlin/com/seedcompany/cordtables/common/GraphQL.kt
+++ api/src/main/kotlin/com/seedcompany/cordtables/common/GraphQL.kt
@@ -1,7 +1,12 @@
 package com.seedcompany.cordtables.common
 
+import com.fasterxml.jackson.annotation.JsonFormat
+import java.lang.reflect.Field
 import java.util.*
 
+
+// -------------------------------------------------------
+// Value
 data class ValueInt(
     val value: Int? = null,
 )
@@ -22,10 +27,24 @@ data class ValueArray(
     val value: Array<String> = arrayOf<String>(),
 )
 
-data class EthnologueCode(
-    val code: ValueString? = null,
+data class ValueFieldZone(
+    val value: FieldZone? = null,
 )
 
+data class ValueUser(
+    val value: GUser? = null,
+)
+
+data class ValueFieldRegion(
+    val value: FieldRegion? = null,
+)
+
+data class ValueFundingAccount(
+    val value: FundingAccount? = null,
+)
+
+// -------------------------------------------------------
+// Objects
 data class Ethnologue(
     val code: ValueString? = null,
     val name: ValueString? = null,
@@ -33,6 +52,37 @@ data class Ethnologue(
     val provisionalCode: ValueString? = null,
     val sensitivity: String? = null,
 )
+
+data class FundingAccount(
+    val id: String,
+    val name: ValueString? = null,
+    val accountNumber: ValueString? = null,
+)
+
+data class FieldRegion(
+    val id: String,
+    val name: ValueString? = null,
+    val fieldZone: ValueFieldZone? = null,
+    val director: ValueUser? = null
+)
+
+data class FieldZone(
+    val id: String,
+    val director: ValueUser? = null,
+    val name: ValueString? = null,
+)
+
+data class GUser(
+    val id: String,
+)
+
+data class IsoCountry(
+    val numeric: Int? = null,
+    val country: String? = null,
+    val alpha3: String? = null,
+    val alpha2: String? = null,
+)
+
 
 data class Language(
     val id: String,
@@ -54,10 +104,23 @@ data class Language(
     val ethnologue: Ethnologue? = null,
 )
 
+data class Location(
+    val id: String,
+    val name: ValueString? = null,
+    val type: ValueString? = null,
+    val isoAlpha3: ValueString? = null,
+    val isoCountry: IsoCountry? = null,
+    val fundingAccount: ValueFundingAccount? = null,
+    val defaultFieldRegion: ValueFieldRegion? = null,
+)
+
 data class Project (
     val id: String? = null,
 )
 
+
+// -------------------------------------------------------
+// Lists
 data class Languages(
     val items: List<Language>? = listOf(),
     val total: Int? = null,
@@ -67,9 +130,15 @@ data class Projects (
     val items: List<Project>? = listOf(),
 )
 
+data class Locations(
+    val items: List<Location>? = listOf(),
+    val total: Int? = null,
+)
+
 data class GData (
     val languages: Languages? = null,
     val projects: Projects? = null,
+    val locations: Locations? = null,
 )
 
 data class GResponse (
