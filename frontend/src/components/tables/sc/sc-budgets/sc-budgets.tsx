fd9b98109c54d78753be3892554a45dd89391b2b
happysenior <hiroshi.tan57@gmail.com>
2021-10-29 22:31:54 -0500

    add specific rules for stenciljs

--- frontend/src/components/tables/sc/sc-budgets/sc-budgets.tsx
+++ frontend/src/components/tables/sc/sc-budgets/sc-budgets.tsx
@@ -1,18 +1,16 @@
-import { Component, Host, h } from '@stencil/core';
+import { Component, Host, h } from '@stencil/core'
 
 @Component({
   tag: 'sc-budgets',
   styleUrl: 'sc-budgets.css',
-  shadow: true,
+  shadow: true
 })
 export class ScBudgets {
-
-  render() {
+  render () {
     return (
       <Host>
         <slot></slot>
       </Host>
-    );
+    )
   }
-
 }
