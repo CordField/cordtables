c5122fd9e50ec27bffc7d2a134a033b869d14dba
arcweld <smweld.af@gmail.com>
2021-11-02 14:32:25 -0400

    table renamed to discussion_channels and columns matched to schema

--- api/src/main/kotlin/com/seedcompany/cordtables/components/tables/common/discussion_channels/Delete.kt
+++ api/src/main/kotlin/com/seedcompany/cordtables/components/tables/common/discussion_channels/Delete.kt
@@ -1,4 +1,65 @@
-//package com.seedcompany.cordtables.components.tables.common.`site-text`
-//
-//class Delete {
-//}
+package com.seedcompany.cordtables.components.tables.common.discussion_channels
+
+import com.seedcompany.cordtables.common.ErrorType
+import com.seedcompany.cordtables.common.Utility
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.ResponseBody
+import java.sql.SQLException
+import javax.sql.DataSource
+
+data class CommonChatsDeleteRequest(
+    val id: Int,
+    val token: String?,
+)
+
+data class CommonChatsDeleteResponse(
+    val error: ErrorType,
+    val id: Int?
+)
+
+@CrossOrigin(origins = ["http://localhost:3333", "https://dev.cordtables.com", "https://cordtables.com"])
+@Controller("CommonChatsDelete")
+class Delete(
+    @Autowired
+    val util: Utility,
+    @Autowired
+    val ds: DataSource,
+) {
+    @PostMapping("common-discussion_channels/delete")
+    @ResponseBody
+    fun deleteHandler(@RequestBody req: CommonChatsDeleteRequest): CommonChatsDeleteResponse {
+
+        if (req.token == null) return CommonChatsDeleteResponse(ErrorType.TokenNotFound, null)
+        if(!util.userHasDeletePermission(req.token, "common.discussion_channels"))
+            return CommonChatsDeleteResponse(ErrorType.DoesNotHaveDeletePermission, null)
+
+        println("req: $req")
+        var deletedChatExId: Int? = null
+
+        this.ds.connection.use { conn ->
+            try {
+
+                val deleteStatement = conn.prepareCall(
+                    "delete from common.discussion_channels where id = ? returning id"
+                )
+                deleteStatement.setInt(1, req.id)
+
+                val deleteStatementResult = deleteStatement.executeQuery()
+
+                if (deleteStatementResult.next()) {
+                  deletedChatExId  = deleteStatementResult.getInt("id")
+                }
+            }
+            catch (e:SQLException ){
+                println(e.message)
+
+                return CommonChatsDeleteResponse(ErrorType.SQLDeleteError, null)
+            }
+        }
+        return CommonChatsDeleteResponse(ErrorType.NoError,deletedChatExId)
+    }
+}
--- api/src/main/kotlin/com/seedcompany/cordtables/components/tables/common/discussion_channels/Update.kt
+++ api/src/main/kotlin/com/seedcompany/cordtables/components/tables/common/discussion_channels/Update.kt
@@ -1,4 +1,59 @@
-//package com.seedcompany.cordtables.components.tables.common.`site-text`
-//
-//class Update {
-//}
+package com.seedcompany.cordtables.components.tables.common.discussion_channels
+
+import com.seedcompany.cordtables.common.ErrorType
+import com.seedcompany.cordtables.common.Utility
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.ResponseBody
+import javax.sql.DataSource
+
+data class CommonChatsUpdateRequest(
+    val token: String?,
+    val chat: ChatInput? = null,
+)
+
+data class CommonChatsUpdateResponse(
+        val error: ErrorType,
+)
+
+
+@CrossOrigin(origins = ["http://localhost:3333", "https://dev.cordtables.com", "https://cordtables.com"])
+@Controller("CommonChatsUpdate")
+class Update(
+        @Autowired
+        val util: Utility,
+
+        @Autowired
+        val ds: DataSource,
+) {
+    @PostMapping("common-discussion-channels/update")
+    @ResponseBody
+    fun updateHandler(@RequestBody req: CommonChatsUpdateRequest): CommonChatsUpdateResponse {
+
+        if (req.token == null) return CommonChatsUpdateResponse(ErrorType.TokenNotFound)
+        if (req.chat == null) return CommonChatsUpdateResponse(ErrorType.MissingId)
+        if (req.chat.id == null) return CommonChatsUpdateResponse(ErrorType.MissingId)
+
+        if (req.chat.owning_person != null) util.updateField(
+                token = req.token,
+                table = "common.discussion_channels",
+                column = "owning_person",
+                id = req.chat.id!!,
+                value = req.chat.owning_person,
+        )
+
+        if (req.chat.owning_group != null) util.updateField(
+                token = req.token,
+                table = "common.discussion_channels",
+                column = "owning_group",
+                id = req.chat.id!!,
+                value = req.chat.owning_group,
+        )
+
+        return CommonChatsUpdateResponse(ErrorType.NoError)
+    }
+
+}
