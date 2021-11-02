ddf5d7b5839b039089ef7b5eba9b6570f6eb73fc
Vivek Varma <vivekvarma15516@gmail.com>
2021-11-03 04:04:29 +0530

    calculating distance using gis
    (cherry picked from commit e6d8c8e274a4aee778c48e16f518bde2f81a4e51)

--- api/src/main/resources/sql/helpers/gis.sql
+++ api/src/main/resources/sql/helpers/gis.sql
@@ -1,6 +1,15 @@
-create extension postgis with schema sc;
-alter table sc.languages add column coordinates sc.geography;
--- india centre
-update sc.languages set coordinates = 'SRID=4326;POINT(77.5 12.9)' where id = 13
-select coordinates from sc.languages;
+create extension if not exists postgis with schema sc;
+alter table sc.languages add column if not exists coordinates sc.geography;
+delete from sc.languages;
+insert into sc.languages(id,name,display_name,owning_person,owning_group, created_by, modified_by, coordinates)
+values
+(1,'Kannada', 'KND', 1,1,1,1,'SRID=4326;POINT(77.5 12.9)'),
+(2,'English', 'ENG',1,1,1,1, 'SRID=4326;POINT(-96.1 32.7)');
+
+
+select round(sc.ST_Distance
+(
+	(select coordinates from sc.languages where id = 1)::sc.geography,
+	(select coordinates from sc.languages where id = 2)::sc.geography
+)/1000) || ' km' as distance;
 
