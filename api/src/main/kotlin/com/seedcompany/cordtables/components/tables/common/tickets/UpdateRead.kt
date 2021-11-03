5c6005e6189a8e913da5d017004aed4a29164685
Rhuan Souza <rhuan288souza@gmail.com>
2021-11-03 15:09:02 -0300

    add CRUDL on common.tickets

--- api/src/main/kotlin/com/seedcompany/cordtables/components/tables/common/tickets/UpdateRead.kt
+++ api/src/main/kotlin/com/seedcompany/cordtables/components/tables/common/tickets/UpdateRead.kt
@@ -1,4 +1,66 @@
-//package com.seedcompany.cordtables.components.tables.common.`site-text`
-//
-//class UpdateRead {
-//}
+package com.seedcompany.cordtables.components.tables.common.tickets
+
+import com.seedcompany.cordtables.common.ErrorType
+import com.seedcompany.cordtables.common.Utility
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.ResponseBody
+import javax.sql.DataSource
+
+data class CommonTicketsUpdateReadRequest(
+        val token: String?,
+        val id: Int? = null,
+        val column: String? = null,
+        val value: Any? = null,
+)
+
+data class CommonTicketsUpdateReadResponse(
+        val error: ErrorType,
+        val ticket: CommonTickets? = null,
+)
+
+@CrossOrigin(origins = ["http://localhost:3333", "https://dev.cordtables.com", "https://cordtables.com"])
+@Controller("CommonTicketsUpdateRead")
+class UpdateRead(
+        @Autowired
+        val util: Utility,
+
+        @Autowired
+        val ds: DataSource,
+
+        @Autowired
+        val update: Update,
+
+        @Autowired
+        val read: Read,
+) {
+    @PostMapping("common-tickets/update-read")
+    @ResponseBody
+    fun updateReadHandler(@RequestBody req: CommonTicketsUpdateReadRequest): CommonTicketsUpdateReadResponse {
+
+        val updateResponse = update.updateHandler(
+                CommonTicketsUpdateRequest(
+                        token = req.token,
+                        column = req.column,
+                        id = req.id,
+                        value = req.value,
+                )
+        )
+
+        if (updateResponse.error != ErrorType.NoError) {
+            return CommonTicketsUpdateReadResponse(updateResponse.error)
+        }
+
+        val readResponse = read.readHandler(
+                CommonTicketsReadRequest(
+                        token = req.token,
+                        id = req.id!!
+                )
+        )
+
+        return CommonTicketsUpdateReadResponse(error = readResponse.error, readResponse.ticket)
+    }
+}
--- frontend/src/components/tables/table-root/table-root.tsx
+++ frontend/src/components/tables/table-root/table-root.tsx
@@ -26,6 +26,7 @@ export class TableRoot {
 
         {this.match.params.table === 'sc-languages-ex' && <languages-ex></languages-ex>}
         {this.match.params.table === 'common-organizations' && <common-organizations></common-organizations>}
+        {this.match.params.table === 'common-tickets' && <tickets-table></tickets-table>}
         {this.match.params.table === 'common-scripture-references' && <scripture-references></scripture-references>}
 
         {this.match.params.table === 'sc-languages' && <sc-languages></sc-languages>}
