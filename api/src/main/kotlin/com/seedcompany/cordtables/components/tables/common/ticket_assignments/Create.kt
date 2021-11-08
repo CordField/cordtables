aeda355b2ca47a81867ca7daffdcc05dca40924e
Rhuan Souza <rhuan288souza@gmail.com>
2021-11-08 16:13:49 -0300

    started

--- api/src/main/kotlin/com/seedcompany/cordtables/components/tables/common/ticket_assignments/Create.kt
+++ api/src/main/kotlin/com/seedcompany/cordtables/components/tables/common/ticket_assignments/Create.kt
@@ -1,4 +1,89 @@
-//package com.seedcompany.cordtables.components.tables.common.`site-text`
-//
-//class Create {
-//}
+package com.seedcompany.cordtables.components.tables.common.ticket_assignments
+
+import com.seedcompany.cordtables.common.*
+import com.seedcompany.cordtables.components.tables.sc.languages.Read
+import com.seedcompany.cordtables.components.tables.sc.languages.Update
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.ResponseBody
+import javax.sql.DataSource
+
+
+data class CommonTicketAssignmentCreateRequest(
+        val token: String? = null,
+        val ticket_assignment: CommonTicketAssignmentsInput
+)
+
+data class CommonTicketAssignmentCreateResponse(
+        val error: ErrorType,
+        val id: Int? = null,
+)
+
+@CrossOrigin(origins = ["http://localhost:3333", "https://dev.cordtables.com", "https://cordtables.com"])
+@Controller("CommonTicketAssignmentCreate")
+
+class Create(
+        @Autowired
+        val util: Utility,
+
+        @Autowired
+        val ds: DataSource,
+
+        @Autowired
+        val update: Update,
+
+        @Autowired
+        val read: Read,
+) {
+    val jdbcTemplate: JdbcTemplate = JdbcTemplate(ds)
+
+    @PostMapping("common-ticket-assignment/create")
+    @ResponseBody
+    fun createHandler(@RequestBody req: CommonTicketAssignmentCreateRequest): CommonTicketAssignmentCreateResponse {
+
+        if (req.token == null) return CommonTicketAssignmentCreateResponse(error = ErrorType.InputMissingToken, null)
+
+        // create row with required fields, use id to update cells afterwards one by one
+        val id = jdbcTemplate.queryForObject(
+                """
+            insert into common.ticket_assignment(ticket, person, created_by, modified_by, owning_person, owning_group)
+                values(
+                    ?,
+                    ?,
+                    (
+                      select person 
+                      from admin.tokens 
+                      where token = ?
+                    ),
+                    (
+                      select person 
+                      from admin.tokens 
+                      where token = ?
+                    ),
+                    (
+                      select person 
+                      from admin.tokens 
+                      where token = ?
+                    ),
+                    1
+                )
+            returning id;
+        """.trimIndent(),
+                Int::class.java,
+                req.ticket_assignment.ticket,
+                req.ticket_assignment.person,
+                req.token,
+                req.token,
+                req.token,
+        )
+
+        return CommonTicketAssignmentCreateResponse(error = ErrorType.NoError, id = id)
+    }
+
+
+}
+
