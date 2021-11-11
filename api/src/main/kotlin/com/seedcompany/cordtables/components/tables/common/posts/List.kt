e9f588f252eec4c174292f276533eaa4e5eb1345
Vivek Varma <vivekvarma15516@gmail.com>
2021-11-12 01:34:15 +0530

    frontend pages for common.posts and common.threads

--- api/src/main/kotlin/com/seedcompany/cordtables/components/tables/common/posts/List.kt
+++ api/src/main/kotlin/com/seedcompany/cordtables/components/tables/common/posts/List.kt
@@ -1,4 +1,120 @@
-//package com.seedcompany.cordtables.components.tables.common.`site-text`
-//
-//class List {
-//}
+package com.seedcompany.cordtables.components.tables.common.posts
+
+
+import com.seedcompany.cordtables.common.ErrorType
+import com.seedcompany.cordtables.common.Utility
+import com.seedcompany.cordtables.components.admin.GetSecureListQuery
+import com.seedcompany.cordtables.components.admin.GetSecureListQueryRequest
+import com.seedcompany.cordtables.components.tables.common.threads.Thread
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.ResponseBody
+import java.sql.SQLException
+import javax.sql.DataSource
+
+data class CommonPostsListRequest(
+        val token: String?
+)
+
+data class CommonPostsListResponse(
+        val error: ErrorType,
+        val posts: MutableList<Post>
+)
+
+@CrossOrigin(origins = ["http://localhost:3333", "https://dev.cordtables.com", "https://cordtables.com"])
+@Controller("CommonPostsList")
+class List(
+        @Autowired
+        val util: Utility,
+
+        @Autowired
+        val ds: DataSource,
+
+        @Autowired
+        val secureList: GetSecureListQuery,
+){
+    var jdbcTemplate: NamedParameterJdbcTemplate = NamedParameterJdbcTemplate(ds)
+
+    @PostMapping("common-posts/list")
+    @ResponseBody
+    fun listHandler(@RequestBody req: CommonPostsListRequest): CommonPostsListResponse
+    {
+        var data: MutableList<Post> = mutableListOf()
+        if (req.token == null) return CommonPostsListResponse(ErrorType.TokenNotFound, mutableListOf())
+        val paramSource = MapSqlParameterSource()
+        paramSource.addValue("token", req.token)
+        val query = secureList.getSecureListQueryHandler(
+                GetSecureListQueryRequest(
+                        tableName = "common.postss",
+                        filter = "order by id",
+                        columns = arrayOf(
+                                "id",
+                                "content",
+                                "thread",
+                                "created_at",
+                                "created_by",
+                                "modified_at",
+                                "modified_by",
+                                "owning_person",
+                                "owning_group",
+                        )
+                )
+        ).query
+
+        try {
+            val jdbcResult = jdbcTemplate.queryForRowSet(query, paramSource)
+            while (jdbcResult.next()) {
+
+                var id: Int? = jdbcResult.getInt("id")
+                if (jdbcResult.wasNull()) id = null
+
+                var content: String? = jdbcResult.getString("content")
+                if (jdbcResult.wasNull()) content = null
+
+                var thread: Int? = jdbcResult.getInt("thread")
+                if (jdbcResult.wasNull()) thread = null
+
+                var created_at: String? = jdbcResult.getString("created_at")
+                if (jdbcResult.wasNull()) created_at = null
+
+                var created_by: Int? = jdbcResult.getInt("created_by")
+                if (jdbcResult.wasNull()) created_by = null
+
+                var modified_at: String? = jdbcResult.getString("modified_at")
+                if (jdbcResult.wasNull()) modified_at = null
+
+                var modified_by: Int? = jdbcResult.getInt("modified_by")
+                if (jdbcResult.wasNull()) modified_by = null
+
+                var owning_person: Int? = jdbcResult.getInt("owning_person")
+                if (jdbcResult.wasNull()) owning_person = null
+
+                var owning_group: Int? = jdbcResult.getInt("owning_group")
+                if (jdbcResult.wasNull()) owning_group = null
+
+                data.add(
+                        Post(
+                                id = id,
+                                thread = thread,
+                                content = content,
+                                created_at = created_at,
+                                created_by = created_by,
+                                modified_at = modified_at,
+                                modified_by = modified_by,
+                                owning_person = owning_person,
+                                owning_group = owning_group
+                        )
+                )
+            }
+        } catch (e: SQLException) {
+            println("error while listing ${e.message}")
+            return CommonPostsListResponse(ErrorType.SQLReadError, mutableListOf())
+        }
+        return CommonPostsListResponse(ErrorType.NoError, data)
+    }
+}
