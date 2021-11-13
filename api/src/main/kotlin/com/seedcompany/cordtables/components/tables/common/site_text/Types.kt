4e58ba184a97560045153034ff8c9e402657bb78
happysenior <hiroshi.tan57@gmail.com>
2021-11-13 03:47:10 -0500

    update common.site_text types

--- api/src/main/kotlin/com/seedcompany/cordtables/components/tables/common/site_text/Types.kt
+++ api/src/main/kotlin/com/seedcompany/cordtables/components/tables/common/site_text/Types.kt
@@ -1,4 +1,23 @@
-package com.seedcompany.cordtables.components.tables.common.`site-text`
+package com.seedcompany.cordtables.components.tables.common.site_text
 
-class Types {
-}
+data class CommonSiteText(
+        var id: Int? = null,
+
+        val ethnologue: Int? = null,
+        val text_id: String? = null,
+        val text_translation: String? = null,
+
+        val created_at: String? = null,
+        val created_by: Int? = null,
+        val modified_at: String? = null,
+        val modified_by: Int? = null,
+        val owning_person: Int? = null,
+        val owning_group: Int? = null,
+        val coordinates: String? = null,
+)
+
+data class CommonSiteTextInput(
+        val ethnologue: Int,
+        val text_id: String,
+        val text_translation: String,
+)
