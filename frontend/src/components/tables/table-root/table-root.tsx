a68987bad013ca5e80c080a655beba7555a0565e
Rhuan288souza <rhuan288souza@gmail.com>
2021-11-15 17:12:55 -0300

    Full Service on Common.work_records

--- frontend/src/components/tables/table-root/table-root.tsx
+++ frontend/src/components/tables/table-root/table-root.tsx
@@ -35,6 +35,7 @@ export class TableRoot {
         {this.match.params.table === 'common-tickets' && <tickets-table></tickets-table>}
         {this.match.params.table === 'common-ticket-graph' && <ticket-graph></ticket-graph>}
         {this.match.params.table === 'common-ticket-assignments' && <ticket-assignments></ticket-assignments>}
+        {this.match.params.table === 'common-work-records' && <work-records></work-records>}
         {this.match.params.table === 'common-scripture-references' && <scripture-references></scripture-references>}
         {this.match.params.table === 'sc-locations' && <sc-locations></sc-locations>}
         {this.match.params.table === 'common-cell-channels' && <common-cell-channels></common-cell-channels>}
