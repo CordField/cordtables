14ae95296fb60b6882bc0aaba1076d8b3461083d
Michael Marshall <michaelmarshall220@gmail.com>
2021-11-16 14:11:08 -0600

    added text column

--- api/src/main/resources/sql/schemas/common/common.schema.sql
+++ api/src/main/resources/sql/schemas/common/common.schema.sql
@@ -27,6 +27,7 @@ create table common.site_text_strings(
   id serial primary key,
 
   english varchar(64) not null, -- US English, all translations including other English locales will be in the translation table
+  comment text,
 
   created_at timestamp not null default CURRENT_TIMESTAMP,
   created_by int not null references admin.people(id),
