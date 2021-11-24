f21f445962efd4977f01105bf958058ae867ab4c
arcweld <smweld.af@gmail.com>
2021-11-24 14:59:15 -0600

    start for budget records

--- api/src/main/kotlin/com/seedcompany/cordtables/components/tables/sc/budget_records/Types.kt
+++ api/src/main/kotlin/com/seedcompany/cordtables/components/tables/sc/budget_records/Types.kt
@@ -1,4 +1,25 @@
-//package com.seedcompany.cordtables.components.tables.common.`site-text`
-//
-//class Types {
-//}
+package com.seedcompany.cordtables.components.tables.sc.budget_records
+
+data class BudgetRecord(
+    var id: Int? = null,
+    val neo4j_id: String? = null,
+
+    var budget: Int? = null,
+    var change_to_plan: Int? = null,
+    var active: Boolean? = null,
+    var amount: Double? = null,
+    var fiscal_year: Int? = null,
+    var partnership: Int? = null,
+
+    val created_at: String? = null,
+    val created_by: Int? = null,
+    val modified_at: String? = null,
+    val modified_by: Int? = null,
+    val owning_person: Int? = null,
+    val owning_group: Int? = null,
+    val peer: Int? = null,
+)
+
+data class BudgetRecordInput(
+    var id: Int? = null,
+)
