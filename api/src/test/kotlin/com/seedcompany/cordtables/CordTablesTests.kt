4c09a0106c8fa5e9708c083f0fdcbbd3793252b2
Michael Marshall <michaelmarshall220@gmail.com>
2021-11-24 23:34:49 -0600

    commented out wip test code

--- api/src/test/kotlin/com/seedcompany/cordtables/CordTablesTests.kt
+++ api/src/test/kotlin/com/seedcompany/cordtables/CordTablesTests.kt
@@ -97,46 +97,54 @@ class CordTablesTests(
 
     @Test
     fun `doesThisWork`() {
-        container.webDriver["http://host.testcontainers.internal:$port/login"]
-//        container.webDriver.get("http://172.17.0.1:3333/")
-        var test = container.webDriver.executeScript("return document.querySelector('%s')")
-
-//        println(test)
-//        container.waitingFor()
-//        val firstResult = WebDriverWait(container.webDriver, 20)
-//            .until(ExpectedConditions.elementToBeSelected(By.name("q")))
-
-        // val wait: Wait<WebDriver> = WebDriverWait(container.webDriver, 10)
-
-        val allElements = container.webDriver.findElementsByXPath(".//*")
-
-        for(elm in allElements){
-            println(elm.tagName)
-            if (elm.tagName == "input"){
-                println(elm.getAttribute("name"))
-            }
-        }
-
-        //wait.until(checkAppRoot(container.webDriver))
-
-        // println("Wat Start")
-        // container.webDriver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS)
-        // container.webDriver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS)
-
-
-//
-//
-//        println(firstResult)
-
-        // val select = container.webDriver.findElementById("tables")
-//        println(select.tagName.toString())
-
-        //val className = messageElement.getAttribute("class")
-       // println(className)
-        //println("app-root: ${messageElement.toString()}")
+        container.webDriver["http://host.testcontainers.internal:$port/"]
+        val messageElement = container.webDriver.findElementByTagName("app-root")
+        println("app-root: ${messageElement.toString()}")
         assert(true)
     }
 
+    // WIP, not ready:)
+//    @Test
+//    fun `doesThisWork2`() {
+//        container.webDriver["http://host.testcontainers.internal:$port/login"]
+////        container.webDriver.get("http://172.17.0.1:3333/")
+//        var test = container.webDriver.executeScript("return document.querySelector('%s')")
+//
+////        println(test)
+////        container.waitingFor()
+////        val firstResult = WebDriverWait(container.webDriver, 20)
+////            .until(ExpectedConditions.elementToBeSelected(By.name("q")))
+//
+//        // val wait: Wait<WebDriver> = WebDriverWait(container.webDriver, 10)
+//
+//        val allElements = container.webDriver.findElementsByXPath(".//*")
+//
+//        for(elm in allElements){
+//            println(elm.tagName)
+//            if (elm.tagName == "input"){
+//                println(elm.getAttribute("name"))
+//            }
+//        }
+//
+//        //wait.until(checkAppRoot(container.webDriver))
+//
+//        // println("Wat Start")
+//        // container.webDriver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS)
+//        // container.webDriver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS)
+//
+//
+////
+////
+////        println(firstResult)
+//
+//        // val select = container.webDriver.findElementById("tables")
+////        println(select.tagName.toString())
+//
+//        //val className = messageElement.getAttribute("class")
+//       // println(className)
+//        //println("app-root: ${messageElement.toString()}")
+//        assert(true)
+//    }
 
     fun checkAppRoot(webDriver: WebDriver):Boolean {
         val  rootElements = webDriver.findElement(By.tagName("app-root")).findElements(By.tagName("div"))
