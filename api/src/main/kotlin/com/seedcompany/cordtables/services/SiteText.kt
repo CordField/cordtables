73e54873bfef186b3dd88eea54bf04c93d2df4a2
happysenior <hiroshi.tan57@gmail.com>
2021-11-26 00:49:07 +0900

    site text translations list API update

--- api/src/main/kotlin/com/seedcompany/cordtables/services/SiteText.kt
+++ api/src/main/kotlin/com/seedcompany/cordtables/services/SiteText.kt
@@ -1,68 +1,88 @@
 package com.seedcompany.cordtables.services
 
 import com.seedcompany.cordtables.common.ErrorType
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Service
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import java.sql.SQLException
 import javax.sql.DataSource
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.decodeFromString
 
 data class SiteTextLanguage(
-    val common_id: Int,
-    val language_name: String? = null,
+        val language: Int,
+        val language_name: String? = null,
 )
 
 data class SiteTextString(
-    val id: Int,
-    val english: String,
-    val comment: String?
+        val id: Int,
+        val english: String,
+        val comment: String?
+)
+
+data class SiteTextTranslation(
+        val language: Int,
+        val translations: MutableList<SiteTextTranslationExceptLanguage>?
+)
+
+@Serializable
+data class SiteTextTranslationExceptLanguage(
+        val id: Int,
+        val site_text: Int,
+        val translation: String
 )
 
 open class SiteTextLanguageResponse(
-    val error: ErrorType,
-    val data: MutableList<SiteTextLanguage>?,
+        val error: ErrorType,
+        val data: MutableList<SiteTextLanguage>?,
 )
 
 open class SiteTextStringResponse(
-    val error: ErrorType,
-    val data: MutableList<SiteTextString>?,
+        val error: ErrorType,
+        val data: MutableList<SiteTextString>?,
+)
+
+open class SiteTextTranslationResponse(
+        val error: ErrorType,
+        val data: MutableList<SiteTextTranslation>?,
 )
 
 @Service
 class SiteTextService(
-    @Autowired
-    val ds: DataSource,
+        @Autowired
+        val ds: DataSource,
 
-) {
+        ) {
     var jdbcTemplate: NamedParameterJdbcTemplate = NamedParameterJdbcTemplate(ds)
 
     fun getSiteTextLanguages(): SiteTextLanguageResponse {
-        var data: MutableList<SiteTextLanguage> = mutableListOf()
+        val data: MutableList<SiteTextLanguage> = mutableListOf()
 
         val paramSource = MapSqlParameterSource()
 
         val query = """
-            select li.common_id, li.name
+            select li.common_id as language, li.name
             from (
-                select distinct(stt.language) as common_id
+                select distinct(stt.language) as language
                 from common.site_text_translations stt
             ) as stl
-            inner join sil.language_index li on li.common_id = stl.common_id
+            inner join sil.language_index li on li.common_id = stl.language
         """.replace('\n', ' ')
 
         try {
             val jdbcResult = jdbcTemplate.queryForRowSet(query, paramSource)
             while (jdbcResult.next()) {
 
-                var common_id: Int = jdbcResult.getInt("common_id")
+                val language: Int = jdbcResult.getInt("language")
 
                 var language_name: String? = jdbcResult.getString("name")
                 if (jdbcResult.wasNull()) language_name = null
 
                 data.add(
                         SiteTextLanguage(
-                                common_id = common_id,
+                                language = language,
                                 language_name = language_name
                         )
                 )
@@ -75,7 +95,7 @@ class SiteTextService(
     }
 
     fun getSiteTextStrings(): SiteTextStringResponse {
-        var data: MutableList<SiteTextString> = mutableListOf()
+        val data: MutableList<SiteTextString> = mutableListOf()
 
         val paramSource = MapSqlParameterSource()
 
@@ -88,7 +108,7 @@ class SiteTextService(
             val jdbcResult = jdbcTemplate.queryForRowSet(query, paramSource)
             while (jdbcResult.next()) {
 
-                var id: Int = jdbcResult.getInt("id")
+                val id: Int = jdbcResult.getInt("id")
 
                 var english: String? = jdbcResult.getString("english")
                 if (jdbcResult.wasNull()) english = null
@@ -98,9 +118,9 @@ class SiteTextService(
 
                 data.add(
                         SiteTextString(
-                            id = id,
-                            english = english!!,
-                            comment = comment
+                                id = id,
+                                english = english!!,
+                                comment = comment
                         )
                 )
             }
@@ -109,5 +129,48 @@ class SiteTextService(
             return SiteTextStringResponse(ErrorType.SQLReadError, mutableListOf())
         }
         return SiteTextStringResponse(ErrorType.NoError, data)
+    }
+
+    fun getSiteTextTranslations(): SiteTextTranslationResponse {
+        val data: MutableList<SiteTextTranslation> = mutableListOf()
+
+        val paramSource = MapSqlParameterSource()
+
+        val query = """
+            select
+                stt.language,
+                coalesce(
+                    jsonb_agg(
+                        jsonb_build_object(
+                            'id', stt.id,
+                            'site_text', stt.site_text,
+                            'translation', stt.translation
+                        )
+                    ) FILTER (WHERE stt.id IS NOT NULL), '[]') AS translations
+            from common.site_text_translations stt
+            group by stt.language
+        """.replace('\n', ' ')
+
+        try {
+            val jdbcResult = jdbcTemplate.queryForRowSet(query, paramSource)
+            while (jdbcResult.next()) {
+
+                val language: Int = jdbcResult.getInt("language")
+
+                val translations: String = jdbcResult.getObject("translations").toString()
+
+                val translationsObject = Json.decodeFromString<MutableList<SiteTextTranslationExceptLanguage>>(translations)
+                data.add(
+                        SiteTextTranslation(
+                                language = language,
+                                translations = translationsObject
+                        )
+                )
+            }
+        } catch (e: SQLException) {
+            println("error while listing ${e.message}")
+            return SiteTextTranslationResponse(ErrorType.SQLReadError, mutableListOf())
+        }
+        return SiteTextTranslationResponse(ErrorType.NoError, data)
     }
 }
