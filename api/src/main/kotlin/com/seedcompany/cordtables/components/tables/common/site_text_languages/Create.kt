55f8982f2d7760d4c5dad2d9304a1e1e308becf9
happysenior <hiroshi.tan57@gmail.com>
2021-12-20 12:58:20 -0300

    site text languages CREATE/READ API done

---
+++ api/src/main/kotlin/com/seedcompany/cordtables/components/tables/common/site_text_languages/Create.kt
@@ -0,0 +1,87 @@
+package com.seedcompany.cordtables.components.tables.common.site_text_languages
+
+import com.seedcompany.cordtables.common.ErrorType
+import com.seedcompany.cordtables.common.Utility
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.ResponseBody
+import java.sql.SQLException
+import javax.sql.DataSource
+
+data class SiteTextLanguageCreateRequest(
+  val token: String? = null,
+  val language: Int,
+)
+
+data class SiteTextLanguageCreateResponse(
+  val error: ErrorType,
+  val id: Int? = null,
+)
+
+@CrossOrigin(origins = ["http://localhost:3333", "https://dev.cordtables.com", "https://cordtables.com", "*"])
+@Controller("CommonSiteTextLanguageCreate")
+class Create(
+  @Autowired
+  val util: Utility,
+
+  @Autowired
+  val ds: DataSource,
+) {
+  val jdbcTemplate: JdbcTemplate = JdbcTemplate(ds)
+
+  @PostMapping("common-site-text-languages/create")
+  @ResponseBody
+  fun createHandler(@RequestBody req: SiteTextLanguageCreateRequest): SiteTextLanguageCreateResponse {
+
+    if (req.token == null) return SiteTextLanguageCreateResponse(error = ErrorType.InputMissingToken, null)
+    if (req.language == null) return SiteTextLanguageCreateResponse(error = ErrorType.InputMissingColumn, null)
+
+    try {
+      val id = jdbcTemplate.queryForObject(
+        """
+            insert into common.site_text_languages(
+                language,
+                created_by, 
+                modified_by, 
+                owning_person, 
+                owning_group)
+            values(
+                ?,
+                (
+                  select person 
+                  from admin.tokens 
+                  where token = ?
+                ),
+                (
+                  select person 
+                  from admin.tokens 
+                  where token = ?
+                ),
+                (
+                  select person 
+                  from admin.tokens 
+                  where token = ?
+                ),
+                1
+            )
+            returning id;
+            """.trimIndent(),
+        Int::class.java,
+        req.language,
+        req.token,
+        req.token,
+        req.token,
+      )
+
+      return SiteTextLanguageCreateResponse(error = ErrorType.NoError, id = id)
+
+    } catch (e: SQLException) {
+      println(e.message)
+      return SiteTextLanguageCreateResponse(ErrorType.SQLInsertError, null)
+    }
+  }
+}
