bc848bf078aa6e5ba16ce7add4457cbc89380a2b
Vivek Varma <vivekvarma15516@gmail.com>
2022-01-18 20:58:02 +0530

    created structure for handlers

--- api/src/main/kotlin/com/seedcompany/cordtables/components/tables/admin/perms/CellPerms.kt
+++ api/src/main/kotlin/com/seedcompany/cordtables/components/tables/admin/perms/CellPerms.kt
@@ -1,5 +1,43 @@
 package com.seedcompany.cordtables.components.tables.admin.perms
 
-class CellPerms {
+import com.seedcompany.cordtables.common.ErrorType
+import com.seedcompany.cordtables.common.Utility
+import com.seedcompany.cordtables.components.admin.GetSecureListQuery
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.ResponseBody
+import javax.sql.DataSource
 
+data class CellPermsRequest(
+  val token: String,
+  val tableName: String,
+  val columnName: String,
+  val rowId: String,
+)
+
+data class CellPermsResponse(
+  val error: ErrorType,
+  val perm: String?,
+)
+
+@CrossOrigin(origins = ["http://localhost:3333", "https://dev.cordtables.com", "https://cordtables.com", "*"])
+@Controller("AdminCellPerms")
+class CellPerms(
+  @Autowired
+  val util: Utility,
+
+  @Autowired
+  val ds: DataSource,
+
+  @Autowired
+  val secureList: GetSecureListQuery
+) {
+  @PostMapping("admin/perms/cell")
+  @ResponseBody
+  fun handler(@RequestBody req: CellPermsRequest): CellPermsResponse{
+    return CellPermsResponse(ErrorType.NoError, null)
+  }
 }
--- api/src/main/kotlin/com/seedcompany/cordtables/components/tables/admin/perms/ColumnPerms.kt
+++ api/src/main/kotlin/com/seedcompany/cordtables/components/tables/admin/perms/ColumnPerms.kt
@@ -1,4 +1,40 @@
 package com.seedcompany.cordtables.components.tables.admin.perms
 
-class ColumnPerms {
+import com.seedcompany.cordtables.common.ErrorType
+import com.seedcompany.cordtables.common.Utility
+import com.seedcompany.cordtables.components.admin.GetSecureListQuery
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.ResponseBody
+import javax.sql.DataSource
+
+data class ColumnPermsRequest(
+  val token:String,
+  val table: String,
+  val column: String,
+)
+
+data class ColumnPermsResponse(
+  val error: ErrorType,
+  val perm: String?
+)
+@CrossOrigin(origins = ["http://localhost:3333", "https://dev.cordtables.com", "https://cordtables.com", "*"])
+@Controller("AdminColumnPerms")
+class ColumnPerms(@Autowired
+                  val util: Utility,
+
+                  @Autowired
+                  val ds: DataSource,
+
+                  @Autowired
+                  val secureList: GetSecureListQuery) {
+
+  @PostMapping("admin/perms/column")
+  @ResponseBody
+  fun handler(@RequestBody req: ColumnPermsRequest): ColumnPermsResponse{
+    return ColumnPermsResponse(ErrorType.NoError, null)
+  }
 }
