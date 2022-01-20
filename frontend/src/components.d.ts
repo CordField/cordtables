c6a9b2f1cfc5fe6b78915f6203c9f98cc8759c72
Michael Marshall <michaelmarshall220@gmail.com>
2022-01-20 17:56:38 -0600

    component file

--- frontend/src/components.d.ts
+++ frontend/src/components.d.ts
@@ -244,6 +244,8 @@ export namespace Components {
     }
     interface ScCeremonies {
     }
+    interface ScChangeToPlan {
+    }
     interface ScChangeToPlans {
     }
     interface ScEthnologue {
@@ -905,6 +907,12 @@ declare global {
         prototype: HTMLScCeremoniesElement;
         new (): HTMLScCeremoniesElement;
     };
+    interface HTMLScChangeToPlanElement extends Components.ScChangeToPlan, HTMLStencilElement {
+    }
+    var HTMLScChangeToPlanElement: {
+        prototype: HTMLScChangeToPlanElement;
+        new (): HTMLScChangeToPlanElement;
+    };
     interface HTMLScChangeToPlansElement extends Components.ScChangeToPlans, HTMLStencilElement {
     }
     var HTMLScChangeToPlansElement: {
@@ -1354,6 +1362,7 @@ declare global {
         "sc-budget-records-partnerships": HTMLScBudgetRecordsPartnershipsElement;
         "sc-budgets": HTMLScBudgetsElement;
         "sc-ceremonies": HTMLScCeremoniesElement;
+        "sc-change-to-plan": HTMLScChangeToPlanElement;
         "sc-change-to-plans": HTMLScChangeToPlansElement;
         "sc-ethnologue": HTMLScEthnologueElement;
         "sc-field-regions-table": HTMLScFieldRegionsTableElement;
@@ -1661,6 +1670,8 @@ declare namespace LocalJSX {
     }
     interface ScCeremonies {
     }
+    interface ScChangeToPlan {
+    }
     interface ScChangeToPlans {
     }
     interface ScEthnologue {
@@ -1886,6 +1897,7 @@ declare namespace LocalJSX {
         "sc-budget-records-partnerships": ScBudgetRecordsPartnerships;
         "sc-budgets": ScBudgets;
         "sc-ceremonies": ScCeremonies;
+        "sc-change-to-plan": ScChangeToPlan;
         "sc-change-to-plans": ScChangeToPlans;
         "sc-ethnologue": ScEthnologue;
         "sc-field-regions-table": ScFieldRegionsTable;
@@ -2040,6 +2052,7 @@ declare module "@stencil/core" {
             "sc-budget-records-partnerships": LocalJSX.ScBudgetRecordsPartnerships & JSXBase.HTMLAttributes<HTMLScBudgetRecordsPartnershipsElement>;
             "sc-budgets": LocalJSX.ScBudgets & JSXBase.HTMLAttributes<HTMLScBudgetsElement>;
             "sc-ceremonies": LocalJSX.ScCeremonies & JSXBase.HTMLAttributes<HTMLScCeremoniesElement>;
+            "sc-change-to-plan": LocalJSX.ScChangeToPlan & JSXBase.HTMLAttributes<HTMLScChangeToPlanElement>;
             "sc-change-to-plans": LocalJSX.ScChangeToPlans & JSXBase.HTMLAttributes<HTMLScChangeToPlansElement>;
             "sc-ethnologue": LocalJSX.ScEthnologue & JSXBase.HTMLAttributes<HTMLScEthnologueElement>;
             "sc-field-regions-table": LocalJSX.ScFieldRegionsTable & JSXBase.HTMLAttributes<HTMLScFieldRegionsTableElement>;
